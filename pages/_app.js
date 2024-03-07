f933189f32827933ca8e95a6b2eab5270191f544
Keiana Hanson <kikihanson94@gmail.com>
2024-03-06 19:40:27 -0500

    intitial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
 import "@/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+const localization = {
+  socialButtonsBlockButton: "Sign In with HEY",
+  unstable__errors: {
+    not_allowed_access:
+      "You cannot pass is - please use another email address",
+  },
+};
+
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ClerkProvider localization={localization} {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 }
+export default MyApp;
